d4725e0dc39c4bfb773d1a15058c9298983aab87
szmisztal <sz.misztal@gmail.com>
2023-08-11 12:33:33 +0200

    add while loop with responses from server

--- client.py
+++ client.py
@@ -3,7 +3,13 @@ import socket as s
 HOST = "127.0.0.1"
 PORT = 65432
 
+BUFFER = 1024
+
 client_socket = s.socket(s.AF_INET, s.SOCK_STREAM)
 client_socket.connect((HOST, PORT))
 
+while True:
+    print(client_socket.recv(BUFFER).decode("utf8"))
+    command_choose = input("Choose command: ").encode("utf8")
+    client_socket.send(command_choose)
 
