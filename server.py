a9cd6fdc2de0a2cab58d03f5e5a1a1c1e43f6b24
szmisztal <sz.misztal@gmail.com>
2023-08-11 11:33:32 +0200

    commit

--- server.py
+++ server.py
@@ -1,1 +1,12 @@
+import socket as s
 
+HOST = "127.0.0.1"
+PORT = 65432
+
+server_socket = s.socket(s.AF_INET, s.SOCK_STREAM)
+server_socket.bind((HOST, PORT))
+server_socket.listen(1)
+
+while True:
+    client_socket, address = server_socket.accept()
+    print(f"You have connection from {address[0]}:{address[1]}")
