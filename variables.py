81475e96d71cd2da015a278441ca382e448704d5
szmisztal <sz.misztal@gmail.com>
2023-08-12 12:10:27 +0200

    add file with all variables

---
+++ variables.py
@@ -0,0 +1,9 @@
+from datetime import datetime as dt
+
+HOST = "127.0.0.1"
+PORT = 65432
+BUFFER = 1024
+utf8 = "UTF-8"
+server_version = "1.0.0"
+server_start_date = "12.08.2023"
+server_uptime = dt.now()
