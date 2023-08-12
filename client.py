809150ced8fb77bffc3793638765d624bdb89cfe
szmisztal <sz.misztal@gmail.com>
2023-08-12 12:10:57 +0200

    change file structure to functions

--- client.py
+++ client.py
@@ -1,15 +1,25 @@
 import socket as s
-
-HOST = "127.0.0.1"
-PORT = 65432
-
-BUFFER = 1024
+import json
+from variables import HOST, PORT, BUFFER, utf8
 
 client_socket = s.socket(s.AF_INET, s.SOCK_STREAM)
 client_socket.connect((HOST, PORT))
 
+def deserialize_json(data):
+    deserialized_data = json.loads(data)
+    return deserialized_data
+
+def receive_request_from_server():
+    server_request = client_socket.recv(BUFFER).decode(utf8)
+    return deserialize_json(server_request)
+
+def response_to_server():
+    response = input("Choose command: uptime / info / help / stop \n").encode(utf8)
+    return client_socket.send(response)
+
 while True:
-    print(client_socket.recv(BUFFER).decode("utf8"))
-    command_choose = input("Choose command: ").encode("utf8")
-    client_socket.send(command_choose)
+    print(response_to_server())
+    print(receive_request_from_server())
+
+
 
