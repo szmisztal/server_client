dd1ee5f5989a6ed3fcfd41c9120b1af0bef63825
szmisztal <sz.misztal@gmail.com>
2023-08-16 16:43:55 +0200

    move response_to_client function to communication_utils.py

--- server.py
+++ server.py
@@ -1,8 +1,10 @@
+from communication_utils import response_to_client
 from network_utils import server_socket_create
-from variables import HOST, PORT, BUFFER, utf8
 from models import Command
+from variables import HOST, PORT, BUFFER, utf8, server_start_time
 
 server_socket = server_socket_create(HOST, PORT)
+command = Command()
 
 while True:
     client_socket, address = server_socket.accept()
@@ -11,12 +13,11 @@ while True:
         client_request = client_socket.recv(BUFFER).decode(utf8)
         if not client_request:
             break
-        command = Command(client_request)
         print(f"Client request: {client_request}")
         if client_request == "stop":
             break
         else:
-            response_data = command.response_to_client(client_request)
+            response_data = response_to_client(command, client_request, server_start_time)
             client_socket.send(response_data)
     print("Server closed")
     client_socket.close()
