2db11f0d9ba171f0d4a0bc940cb0b5244e1ce2dd
szmisztal <sz.misztal@gmail.com>
2023-08-16 16:44:40 +0200

    create functions to write/read datas to json files

--- data_utils.py
+++ data_utils.py
@@ -6,3 +6,12 @@ def serialize_json(data):
 
 def deserialize_json(data):
     return json.loads(data)
+
+def write_to_json_file(file, data):
+    with open(file, "w") as file:
+        json.dump(data, file)
+
+def read_json_file(file):
+    with open(file, "r") as file:
+        json_object = json.load(file)
+    return json_object
