26d932794daa54624dd14876a46d57cf12eeb2b5
szmisztal <sz.misztal@gmail.com>
2023-08-17 16:59:45 +0200

    delete register_user function

--- communication_utils.py
+++ communication_utils.py
@@ -1,7 +1,5 @@
 from data_utils import serialize_json
 from variables import utf8
-from models import User
-
 
 def request_to_server():
     request = input("Choose command: uptime / info / help / stop \n").encode(utf8)
@@ -18,14 +16,5 @@ def response_to_client(self, client_request, server_start_time):
         error_msg = {
             "Unknown command": f"'{client_request}', try again"
         }
-        return serialize_json(error_msg)
+        return serialize_json(error_msg).encode(utf8)
 
-def register_user():
-    username = input("Choose your username: ")
-    password = input("Set your password: ")
-    user = User(username, password)
-    user_dict = {
-        "Username": f"{user.username}",
-        "Password": f"{user.password}"
-    }
-    return user, serialize_json(user_dict)
