803f1d3b64e938313950ac46731041653ad2b613
szmisztal <sz.misztal@gmail.com>
2023-08-19 17:10:33 +0200

    delete encode function

--- communication_utils.py
+++ communication_utils.py
@@ -1,20 +1,31 @@
-from data_utils import serialize_json
-from variables import utf8
+from data_utils import serialize_json, deserialize_json
+from models import User
+
 
 def request_to_server():
-    request = input("Choose command: uptime / info / help / stop \n").encode(utf8)
+    request = input("Choose command: register / uptime / info / help / stop \n")
     return request
 
-def response_to_client(self, client_request, server_start_time):
-    if client_request == "uptime":
-        return self.uptime(server_start_time)
-    elif client_request == "info":
-        return self.info()
-    elif client_request == "help":
-        return self.help()
+def response_to_client(client_request, **kwargs):
+    command = kwargs.get("command")
+    if command:
+        if client_request == "uptime":
+            server_start_time = kwargs.get("server_start_time")
+            return command.uptime(server_start_time)
+        elif client_request == "info":
+            return command.info()
+        elif client_request == "help":
+            return command.help()
+    elif client_request == "register":
+        registration_data = kwargs.get("registration_data")
+        registration_data_dict = deserialize_json(registration_data)
+        return User.register_user(registration_data_dict)
     else:
         error_msg = {
             "Unknown command": f"'{client_request}', try again"
         }
-        return serialize_json(error_msg).encode(utf8)
+        return serialize_json(error_msg)
+
+
+
 
