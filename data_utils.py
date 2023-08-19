fcc7f66aba9bafa4b643e15b0a9002b3596e11f8
szmisztal <sz.misztal@gmail.com>
2023-08-19 17:11:31 +0200

    delete .encode function

--- data_utils.py
+++ data_utils.py
@@ -2,16 +2,17 @@ import json
 from variables import utf8
 
 def serialize_json(data):
-    return json.dumps(data).encode(utf8)
+    return json.dumps(data)
 
 def deserialize_json(data):
     return json.loads(data)
 
-def write_to_json_file(file, data):
-    with open(file, "a") as file:
-        json.dump(data, file)
+def write_to_json_file(filename, data):
+    with open(filename, "a") as file:
+        json.dump(data, file, indent = 4)
 
-def read_json_file(file):
-    with open(file, "r") as file:
+def read_json_file(filename):
+    with open(filename, "r") as file:
         json_object = json.load(file)
     return json_object
+
