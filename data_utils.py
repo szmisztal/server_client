53c9269a1d28e33bf17a58a5752ce565af094352
szmisztal <sz.misztal@gmail.com>
2023-08-20 15:38:53 +0200

    add input funciton with questions about username and password

--- data_utils.py
+++ data_utils.py
@@ -1,5 +1,5 @@
 import json
-from variables import utf8
+
 
 def serialize_json(data):
     return json.dumps(data)
@@ -8,11 +8,19 @@ def deserialize_json(data):
     return json.loads(data)
 
 def write_to_json_file(filename, data):
-    with open(filename, "a") as file:
+    with open(filename, "w") as file:
         json.dump(data, file, indent = 4)
 
 def read_json_file(filename):
     with open(filename, "r") as file:
-        json_object = json.load(file)
-    return json_object
+        users_data = json.load(file)
+    return users_data
 
+def user_username_and_password_input():
+    username = input("Username: ")
+    password = input("Password: ")
+    user_data = {
+        "username": username,
+        "password": password
+    }
+    return user_data
