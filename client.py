23f22a2c1a3ac3ada375ce03e23cf183f5df04a6
szmisztal <sz.misztal@gmail.com>
2023-08-20 17:25:01 +0200

    add possible to login user

--- client.py
+++ client.py
@@ -2,12 +2,14 @@ from network_utils import client_socket_create
 from communication_utils import request_to_server
 from data_utils import serialize_json, deserialize_json, user_username_and_password_input
 from variables import HOST, PORT, BUFFER, utf8
+from models import User
 
 
 client_socket = client_socket_create(HOST, PORT)
+user = User("", "")
 
 while True:
-    request = request_to_server().encode(utf8)
+    request = request_to_server(user).encode(utf8)
     client_socket.send(request)
     if request.decode(utf8) == "stop":
         print("Client closed")
@@ -17,9 +19,16 @@ while True:
         user_data = user_username_and_password_input()
         user_json_data = serialize_json(user_data).encode(utf8)
         client_socket.send(user_json_data)
-    response = deserialize_json(client_socket.recv(BUFFER))
-    for key, value in response.items():
-        print(f"{key} : {value}")
+        response = deserialize_json(client_socket.recv(BUFFER))
+        for key, value in response.items():
+            print(f"{key} : {value}")
+        if "Message" in response and "logged in" in response["Message"]:
+            user = User(**user_data)
+            user.logged_in = True
+    else:
+        response = deserialize_json(client_socket.recv(BUFFER))
+        for key, value in response.items():
+            print(f"{key}: {value}")
 
 
 
