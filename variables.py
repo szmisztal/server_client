466fc7b287890a371f78ea5b37ecbf8531a8d039
szmisztal <sz.misztal@gmail.com>
2023-08-20 18:36:02 +0200

    change server version

--- variables.py
+++ variables.py
@@ -4,12 +4,10 @@ HOST = "127.0.0.1"
 PORT = 65432
 BUFFER = 1024
 utf8 = "UTF-8"
-server_version = "0.1.2"
+server_version = "0.1.3"
 server_start_date = "12.08.2023"
 server_status = True
 server_start_time = dt.now()
 users_file = "users.json"
-users_list = []
-logged_users = []
 
 
