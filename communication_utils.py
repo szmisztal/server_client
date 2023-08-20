773762086644eed0445d1df3eca70fd003c68389
szmisztal <sz.misztal@gmail.com>
2023-08-20 18:35:07 +0200

    add logout option to response_to_client)

--- communication_utils.py
+++ communication_utils.py
@@ -1,11 +1,12 @@
-from data_utils import serialize_json, deserialize_json
+from data_utils import serialize_json, deserialize_json, read_json_file
 from models import User
+from variables import users_file
 
 
 def request_to_server(user):
     print(user)
-    if user.is_logged_in() == True:
-        request = input("Choose command: uptime / info / help / stop \n")
+    if user.logged_in == True:
+        request = input("Choose command: uptime / info / help / logout / stop \n")
         return request
     else:
         request = input("Choose command: register / login / stop \n")
@@ -27,13 +28,30 @@ def response_to_client(client_request, **kwargs):
     elif client_request == "login":
         login_data = kwargs.get("login_data")
         login_data_dict = deserialize_json(login_data)
-        # user = User(**login_data_dict)
-        # user.logged_in = True
-        # print(user)
-        return user.login_user(login_data_dict)
+        return login_user(login_data_dict)
+    elif client_request == "logout":
+        return command.logout()
     else:
         error_msg = {
             "Unknown command": f"'{client_request}', try again"
         }
         return serialize_json(error_msg)
+
+def login_user(login_data_dict):
+    username = login_data_dict["username"]
+    password = login_data_dict["password"]
+    users_list = read_json_file(users_file)
+    for user_data in users_list:
+        stored_username = user_data["username"]
+        stored_password = user_data["password"]
+        if username == stored_username and password == stored_password:
+            login_msg = {
+                "Message": "You was logged in"
+            }
+            return serialize_json(login_msg)
+    error_msg = {
+        "Message": "Incorrect data. try again"
+    }
+    return serialize_json(error_msg)
+
 
