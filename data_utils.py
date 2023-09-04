1fca0890ffac70858daa17568d1deea4ca58f93c
szmisztal <sz.misztal@gmail.com>
2023-09-04 12:36:34 +0200

    delete write/read_json functions, refactor project

--- data_utils.py
+++ data_utils.py
@@ -1,22 +1,11 @@
 import json
+from variables import encode_format
 
-
-def serialize_json(data):
-    return json.dumps(data)
+def serialize_to_json(data):
+    return json.dumps(data).encode(encode_format)
 
 def deserialize_json(data):
     return json.loads(data)
 
-def write_to_json_file(filename, data):
-    with open(filename, "w") as file:
-        json.dump(data, file, indent = 4)
-
-def read_json_file(filename):
-    try:
-        with open(filename, "r") as file:
-            users_data = json.load(file)
-    except (FileNotFoundError, json.JSONDecodeError):
-        users_data = []
-    return users_data
 
 
