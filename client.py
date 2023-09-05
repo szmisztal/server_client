c857663d81d0bf9088c7eac699f73604295603cf
szmisztal <sz.misztal@gmail.com>
2023-09-05 11:38:58 +0200

    add stop def

--- client.py
+++ client.py
@@ -21,22 +21,36 @@ class Client:
 
     def make_request_to_server(self, client_input):
         request_to_server = {
-            "Command": client_input
+            "Request": client_input
         }
         return request_to_server
+
+    def read_response_dict(self, dict_data):
+        deserialized_dict = self.deserialize_json(dict_data)
+        for key, value in deserialized_dict.items():
+            print(f"{key}: {value}")
 
     def start(self):
         with s.socket(INTERNET_ADDRESS_FAMILY, SOCKET_TYPE) as client_socket:
             client_socket.connect((HOST, PORT))
             server_response = client_socket.recv(self.BUFFER)
-            print(self.deserialize_json(server_response))
+            self.read_response_dict(server_response)
             while self.is_running:
                 client_input = input()
                 client_request = self.make_request_to_server(client_input)
                 client_request_json = self.serialize_to_json(client_request)
                 client_socket.sendall(client_request_json)
                 server_response = client_socket.recv(self.BUFFER)
-                print(self.deserialize_server_response(server_response))
+                self.read_response_dict(server_response)
+                self.stop(server_response)
+
+    def stop(self, dict_data):
+        deserialized_dict = self.deserialize_json(dict_data)
+        if "Server status" in deserialized_dict and "Shutting down" in deserialized_dict["Server status"]:
+            print("CLIENT CLOSED...")
+            self.is_running = False
+        else:
+            pass
 
 
 if __name__ == "__main__":
