72b3f017746653e06742982a37644fdda52e6b07
szmisztal <sz.misztal@gmail.com>
2023-09-06 14:17:07 +0200

    little changes in def start, and add new __init__ connections with CommunicationUtils and DataUtils

--- server.py
+++ server.py
@@ -1,7 +1,7 @@
 import socket as s
-import json
 from datetime import datetime as dt
 from communication_utils import CommunicationUtils
+from data_utils import DataUtils
 from variables import HOST, PORT, INTERNET_ADDRESS_FAMILY, SOCKET_TYPE, BUFFER, encode_format
 
 
@@ -13,17 +13,12 @@ class Server:
         self.SOCKET_TYPE = SOCKET_TYPE
         self.BUFFER = BUFFER
         self.encode_format = encode_format
-        self.communication_utils = CommunicationUtils(self)
+        self.communication_utils = CommunicationUtils(self, "")
+        self.data_utils = DataUtils()
         self.is_running = True
         self.server_start_date = "12.08.2023"
         self.server_version = "0.1.5"
         self.server_start_time = dt.now()
-
-    def serialize_to_json(self, dict_data):
-        return json.dumps(dict_data).encode(encode_format)
-
-    def deserialize_json(self, dict_data):
-        return json.loads(dict_data)
 
     def first_message_to_client(self):
         start_message = {
@@ -32,10 +27,14 @@ class Server:
         return start_message
 
     def read_client_request(self, client_request):
-        deserialized_dict = self.deserialize_json(client_request)
+        deserialized_dict = self.data_utils.deserialize_json(client_request)
         print(deserialized_dict)
-        request = deserialized_dict["Request"]
-        return request
+        if "Request" in deserialized_dict:
+            request = deserialized_dict["Request"]
+            return request
+        elif "Register" in deserialized_dict:
+            user_data = deserialized_dict
+            return user_data
 
     def start(self):
         with s.socket(self.INTERNET_ADDRESS_FAMILY, self.SOCKET_TYPE) as server_socket:
@@ -45,14 +44,14 @@ class Server:
             client_ip = address[0]
             client_port = address[1]
             print(f"Connection from {client_ip}:{client_port}")
-            welcome_message = self.serialize_to_json(self.first_message_to_client())
+            welcome_message = self.data_utils.serialize_to_json(self.first_message_to_client())
             client_socket.sendall(welcome_message)
             with client_socket:
                 while self.is_running:
                     client_request_json = client_socket.recv(self.BUFFER)
                     client_request = self.read_client_request(client_request_json)
                     response_to_client = self.communication_utils.response_to_client(client_request)
-                    response_to_client_json = self.serialize_to_json(response_to_client)
+                    response_to_client_json = self.data_utils.serialize_to_json(response_to_client)
                     client_socket.sendall(response_to_client_json)
 
     def stop(self):
