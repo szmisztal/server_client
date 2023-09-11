add41bdaf084348f525c3ec54728f8bba7137655
szmisztal <sz.misztal@gmail.com>
2023-09-11 13:21:58 +0200

    move command variable and user variable to start response_to_cleint function, to not repeat code

--- communication_utils.py
+++ communication_utils.py
@@ -14,36 +14,35 @@ def request_to_server(user):
                         "--------------------------------------------------------\n")
         return request
 
+def read_server_response(data_dict):
+    for key, value in data_dict.items():
+        print(f">>>>> {key}: {value}")
+
 def response_to_client(client_request, **kwargs):
+    command = kwargs.get("command")
+    user = kwargs.get("user")
     if client_request == "uptime":
-        command = kwargs.get("command")
         server_start_time = kwargs.get("server_start_time")
         return command.uptime(server_start_time)
     elif client_request == "info":
-        command = kwargs.get("command")
         return command.info()
     elif client_request == "help":
-        command = kwargs.get("command")
         return command.help()
     elif client_request in ["admin register", "register"]:
         registration_data = kwargs.get("registration_data")
         registration_data_dict = deserialize_json(registration_data)
         return User.register_user(registration_data_dict)
     elif client_request == "show data":
-        user = kwargs.get("user")
         return user.show_current_data()
     elif client_request  == "login":
-        user = kwargs.get("user")
         login_data = kwargs.get("login_data")
         login_data_dict = deserialize_json(login_data)
         return user.login_user(login_data_dict)
     elif client_request == "change data":
-        user = kwargs.get("user")
         new_data = kwargs.get("new_data")
         new_data_dict = deserialize_json(new_data)
         return user.change_user_data(new_data_dict)
     elif client_request == "send message":
-        user = kwargs.get("user")
         recipient_data = kwargs.get("recipient_data")
         message_data = kwargs.get("message_data")
         if recipient_data and message_data:
@@ -54,13 +53,10 @@ def response_to_client(client_request, **kwargs):
             recipient_data = deserialize_json(recipient_data)
             return serialize_json(recipient_data)
     elif client_request == "inbox":
-        user = kwargs.get("user")
         return user.show_new_messages()
     elif client_request == "archived messages":
-        user = kwargs.get("user")
         return user.show_archived_messages()
     elif client_request == "logout":
-        command = kwargs.get("command")
         return command.logout()
     else:
         error_msg = {
