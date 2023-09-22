c7868f975c424da710fe44e999072195cba4e6ab
szmisztal <sz.misztal@gmail.com>
2023-09-22 12:31:55 +0200

    import psycopg2, create 'create table' function

--- data_utils.py
+++ data_utils.py
@@ -1,6 +1,7 @@
 import json
+import psycopg2
 from variables import encode_format
-
+from secrets import password
 
 class DataUtils:
     def serialize_to_json(self, dict_data):
@@ -8,6 +9,24 @@ class DataUtils:
 
     def deserialize_json(self, dict_data):
         return json.loads(dict_data)
+
+    def create_table(self, name):
+        self.connection = psycopg2.connect(
+            user = "postgres",
+            password = password,
+            host = "127.0.0.1",
+            port = "5432",
+            database = "postgres_db"
+        )
+        cursor = self.connection.cursor()
+        create_table_query = f'''CREATE TABLE {name}
+                        (ID SERIAL PRIMARY KEY,
+                        USERNAME VARCHAR NOT NULL,
+                        PASSWORD VARCHAR NOT NULL,
+                        ADMIN_ROLE BOOLEAN NOT NULL DEFAULT FALSE)'''
+        cursor.execute(create_table_query)
+        self.connection.commit()
+        print("Table was created")
 
     def write_to_json_file(self, filename, data):
         with open(filename, "w") as file:
