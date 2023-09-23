4c03857192c0741bc0b0d3e378e1d03673fac02d
szmisztal <sz.misztal@gmail.com>
2023-09-23 12:41:26 +0200

    add querys for users table and messages table create, make connection_to_db and checks that table exists

--- data_utils.py
+++ data_utils.py
@@ -10,30 +10,59 @@ class DataUtils:
     def deserialize_json(self, dict_data):
         return json.loads(dict_data)
 
-    def create_table(self, name):
-        self.connection = psycopg2.connect(
+    def connection_to_db(self):
+        connection = psycopg2.connect(
             user = "postgres",
             password = password,
             host = "127.0.0.1",
             port = "5432",
-            database = "db"
+            database = "server_client_db"
         )
-        cursor = self.connection.cursor()
-        table_exists_query = f"SELECT EXISTS (SELECT 1 FROM information_schema.tables WHERE table_name = '{name}')"
+        return connection
+
+    def check_that_table_exist(self, cursor, table_name):
+        table_exists_query = f"SELECT EXISTS (SELECT 1 FROM information_schema.tables WHERE table_name = '{table_name}')"
         cursor.execute(table_exists_query)
         table_exists = cursor.fetchone()[0]
-
         if not table_exists:
-            create_table_query = f'''CREATE TABLE {name}
-                            (ID SERIAL PRIMARY KEY,
-                            USERNAME VARCHAR NOT NULL,
-                            PASSWORD VARCHAR NOT NULL,
-                            ADMIN_ROLE BOOLEAN NOT NULL DEFAULT FALSE)'''
-            cursor.execute(create_table_query)
-            self.connection.commit()
-            print("Table was created")
+            return True
         else:
-            print(f"Table {name} already exists")
+            return False
+
+    def create_user_table(self):
+        connection = self.connection_to_db()
+        cursor = connection.cursor()
+        table_status = self.check_that_table_exist(cursor, "users")
+        if table_status == True:
+            create_table_query = '''CREATE TABLE users(
+                                user_id SERIAL PRIMARY KEY,
+                                username VARCHAR NOT NULL,
+                                password VARCHAR NOT NULL,
+                                admin_role BOOLEAN NOT NULL DEFAULT FALSE
+                                )'''
+            cursor.execute(create_table_query)
+            connection.commit()
+            print("TABLE 'users' CREATED...")
+        else:
+            print("TABLE 'users' EXISTS...")
+
+    def create_message_table(self):
+        connection = self.connection_to_db()
+        cursor = connection.cursor()
+        table_status = self.check_that_table_exist(cursor, "messages")
+        if table_status == True:
+            create_table_query = '''CREATE TABLE messages(
+                                message_id SERIAL PRIMARY KEY,
+                                user_id INT REFERENCES users(user_id) ON DELETE CASCADE,
+                                sender VARCHAR NOT NULL,
+                                text VARCHAR(255) NOT NULL,
+                                archived BOOLEAN NOT NULL DEFAULT FALSE
+                                )'''
+            cursor.execute(create_table_query)
+            connection.commit()
+            print("TABLE 'messages' CREATED")
+        else:
+            print("TABLE 'messages' EXISTS")
 
     def write_to_json_file(self, filename, data):
         with open(filename, "w") as file:
