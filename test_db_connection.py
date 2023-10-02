dd1442935386b63059171e79e12220c0a0bceda7
szmisztal <sz.misztal@gmail.com>
2023-10-02 10:43:30 +0200

    add test_db_connection.py file, with connections time tests

---
+++ test_db_connection.py
@@ -0,0 +1,10 @@
+from data_utils import DataUtils
+
+
+data = DataUtils()
+data.test_db_connection_with_close_conn_after_every_query()
+data.test_db_connection_with_close_conn_after_all_query()
+
+
+
+
