77ba1b39e301fd8b755f52fa79347d7d9d451e45
szmisztal <sz.misztal@gmail.com>
2023-10-02 11:11:23 +0200

    now its possible to put number of loop iteration to methods args call

--- test_db_connection.py
+++ test_db_connection.py
@@ -2,8 +2,8 @@ from data_utils import DataUtils
 
 
 data = DataUtils()
-data.test_db_connection_with_close_conn_after_every_query()
-data.test_db_connection_with_close_conn_after_all_query()
+data.test_db_connection_with_close_conn_after_every_query(10000)
+data.test_db_connection_with_close_conn_after_all_query(10000)
 
 
 
