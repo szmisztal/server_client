df9cad4bbed5e00c9b179840bffa5f2c8fad16a6
szmisztal <sz.misztal@gmail.com>
2023-10-26 17:40:18 +0200

    add test_database variable

--- variables.py
+++ variables.py
@@ -7,7 +7,8 @@ BUFFER = 1024
 encode_format = "UTF-8"
 INTERNET_ADDRESS_FAMILY = s.AF_INET
 SOCKET_TYPE = s.SOCK_STREAM
-db_file_name = "server-client_db.db"
+database = "server-client_db.db"
+test_database = "server-client_test_db.db"
 
 
 
