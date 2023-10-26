93b2efdba0f911cd75cc92cb85ce80b6ec12e67e
szmisztal <sz.misztal@gmail.com>
2023-10-26 17:42:25 +0200

    change all methods to work with hash password service

--- users_utils.py
+++ users_utils.py
@@ -1,4 +1,5 @@
-from data_utils import DataUtils
+from data_utils import DataUtils, SQLite, PostgreSQL
+from variables import database
 
 
 class User:
@@ -8,13 +9,16 @@ class User:
         self.admin_role = False
         self.logged_in = False
         self.data_utils = DataUtils()
+        self.sqlite_utils = SQLite(database)
+        self.postgresql_utils = PostgreSQL()
 
     def register_user(self, user_data):
         username = user_data["username"]
         password = user_data["password"]
-        validated_username = self.data_utils.validate_username(username)
+        hashed_password = self.data_utils.hash_password(password)
+        validated_username = self.sqlite_utils.validate_username(username)
         if validated_username == True:
-            self.data_utils.register_user_to_db(username, password)
+            self.sqlite_utils.register_user_to_db(username, hashed_password)
             register_message = {
                 "User": f"{username} registered successfully",
             }
@@ -28,7 +32,7 @@ class User:
     def login_user(self, user_data):
         username = user_data["username"]
         password = user_data["password"]
-        validated_data = self.data_utils.validate_credentials(username, password)
+        validated_data = self.sqlite_utils.validate_credentials(username, password)
         if validated_data == True:
             login_message = {
                 f"User '{username}'": "Sign in successfully"
@@ -47,13 +51,14 @@ class User:
         return logout_message
 
     def delete_user(self, user_confirmation):
-        if user_confirmation not in ["yes", "Yes"]:
+        if user_confirmation != "yes":
             not_deletion_message = {
                 "Confirmation failed": "Your data will remain in the database"
             }
             return not_deletion_message
         else:
-            self.data_utils.delete_user_from_db(self.username, self.password)
+            hashed_password = self.sqlite_utils.get_hashed_password_from_db(self.username)
+            self.sqlite_utils.delete_user_from_db(self.username, hashed_password)
             deletion_message = {
                 "Delete": "You have been deleted from database"
             }
@@ -62,14 +67,16 @@ class User:
     def change_user_data(self, new_user_data):
         new_username = new_user_data["username"]
         new_password = new_user_data["password"]
-        validated_username = self.data_utils.validate_username(new_username)
+        new_hashed_password = self.data_utils.hash_password(new_password)
+        validated_username = self.sqlite_utils.validate_username(new_username)
         if validated_username == False:
             error_message = {
                 "Username": "In use, choose another"
             }
             return error_message
         else:
-            self.data_utils.update_user_data(new_username, new_password, self.username, self.password)
+            hashed_password = self.sqlite_utils.get_hashed_password_from_db(self.username)
+            self.sqlite_utils.update_user_data(new_username, new_hashed_password, self.username, hashed_password)
             change_data_message = {
                 "Success": f"Your new data: username: {new_username}, password: {new_password}"
             }
@@ -78,14 +85,14 @@ class User:
     def send_message(self, recipient_data, message_data):
         recipient = recipient_data
         message = message_data
-        validated_recipient = self.data_utils.validate_username(recipient)
+        validated_recipient = self.sqlite_utils.validate_username(recipient)
         if validated_recipient == True:
             error_message = {
                 "Recipient": "User not found, try again"
             }
             return error_message
         else:
-            recipient_messages_list = self.data_utils.user_messages_list(recipient, False)
+            recipient_messages_list = self.sqlite_utils.user_messages_list(recipient, False)
             if len(message) > 255:
                 error_message = {
                     "Failed": "Max message length = 255"
@@ -98,19 +105,19 @@ class User:
                     }
                     return error_message
             else:
-                self.data_utils.save_message_to_db(self.username, message, recipient)
+                self.sqlite_utils.save_message_to_db(self.username, message, recipient)
                 send_message_success = {
                     "Message": "Send successfully"
                 }
                 return send_message_success
 
     def show_new_messages(self):
-        user_messages_list = self.data_utils.user_messages_list(self.username, False)
-        self.data_utils.archive_messages(self.username)
+        user_messages_list = self.sqlite_utils.user_messages_list(self.username, False)
+        self.sqlite_utils.archive_messages(self.username)
         return user_messages_list
 
     def show_archived_messages(self):
-        archived_messages = self.data_utils.user_messages_list(self.username, True)
+        archived_messages = self.sqlite_utils.user_messages_list(self.username, True)
         return archived_messages
 
     def __str__(self):
