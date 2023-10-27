0b0ed40b2f2a32a9c762a67dabf9da9ba527eb36
szmisztal <sz.misztal@gmail.com>
2023-10-27 10:58:48 +0200

    add variables for different databases settings

--- variables.py
+++ variables.py
@@ -1,4 +1,5 @@
 import socket as s
+from secrets import password
 
 
 HOST = "127.0.0.1"
@@ -7,8 +8,16 @@ BUFFER = 1024
 encode_format = "UTF-8"
 INTERNET_ADDRESS_FAMILY = s.AF_INET
 SOCKET_TYPE = s.SOCK_STREAM
-database = "server-client_db.db"
-test_database = "server-client_test_db.db"
-
+sqlite_database = "sqlite_server-client_db.db"
+sqlite_test_database = "sqlite_server-client_test_db.db"
+postgreSQL_database = "postgreSQL_server_client_db"
+postgreSQL_test_database = "postgreSQL_server_client_test_db"
+postgreSQL_server_connection_dict = {
+    "user": "postgres",
+    "password": password,
+    "host": HOST,
+    "port": PORT,
+    "database": postgreSQL_database
+}
 
 
