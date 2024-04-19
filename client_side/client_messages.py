ca4da79802f1f6596faedd38fb5ea52de169b2a7
szmisztal <sz.misztal@gmail.com>
2024-04-19 10:49:21 +0200

    add send_message_input, add service for it to request_to_server method

--- client_side/client_messages.py
+++ client_side/client_messages.py
@@ -18,14 +18,22 @@ class ClientRequests:
         user_data = self.user_data_input()
         return self.message_template.template(message = command, data = user_data)
 
-    def delete_account_confirmation_input(self):
-        decision = input("Do you really want to delete your account ? Y/N").lower()
-        return self.message_template.template(message = "delete", data = decision)
+    def delete_account_confirmation_input(self, command):
+        decision = input("Do you really want to delete your account ? Y/N ").lower()
+        return self.message_template.template(message = command, data = decision)
+
+    def send_message_input(self, command):
+        recipient = input("Who you want to send the message to ?: ")
+        message = input("Message: ")
+        mail = {"recipient": recipient, "mail": message}
+        return self.message_template.template(message = command, data = mail)
 
     def request_to_server(self):
         command = input("REQUEST: ").lower()
         if command in ["register", "login", "change data"]:
             return self.user_data_handling(command)
         elif command == "delete":
-            return self.delete_account_confirmation_input()
+            return self.delete_account_confirmation_input(command)
+        elif command == "send message":
+            return self.send_message_input(command)
         return self.message_template.template(message = command)
