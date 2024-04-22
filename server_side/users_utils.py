aa3a8517ee6f7b523c483566c71c7f7c62626fc1
szmisztal <sz.misztal@gmail.com>
2024-04-22 14:23:11 +0200

    add exceptions catching on db operations

--- server_side/users_utils.py
+++ server_side/users_utils.py
@@ -1,5 +1,6 @@
 import os
-from data_utils import DataUtils, SQLite
+from server_side.data_utils import DataUtils, SQLite
+from config_variables import logger_config
 
 
 sqlite_db_base_directory = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -13,40 +14,57 @@ class User:
         self.__password = None
         self.admin_role = False
         self.data_utils = DataUtils()
+        self.logger = logger_config("User", os.getcwd(), "server_logs.log")
         self.sqlite_utils = SQLite(sqlite_database_path)
         # self.postgresql_utils = PostgreSQL(**postgreSQL_server_connection_dict)
 
     def register_user(self, user_data):
         username = user_data["username"]
-        validate_username = self.sqlite_utils.validate_username(username)
-        if validate_username is False:
-            password = user_data["password"]
-            self.sqlite_utils.register_user_to_db(username, password)
-            return True
-        return False
+        try:
+            validate_username = self.sqlite_utils.validate_username(username)
+            if validate_username is False:
+                password = user_data["password"]
+                self.sqlite_utils.register_user_to_db(username, password)
+                return True
+            return False
+        except Exception as e:
+            self.logger.debug(f"Error: {e}")
+            return False
 
     def login_user(self, user_data):
         username, password = user_data["username"], user_data["password"]
-        validate_data = self.sqlite_utils.validate_credentials(username, password)
-        if validate_data:
-            self.username, self.__password = username, password
-        return validate_data
+        try:
+            validate_data = self.sqlite_utils.validate_credentials(username, password)
+            if validate_data:
+                self.username, self.__password = username, password
+            return validate_data
+        except Exception as e:
+            self.logger.debug(f"Error: {e}")
+            return False
 
     def change_user_data(self, user_data):
         new_username = user_data["username"]
-        validate_username = self.sqlite_utils.validate_username(new_username)
-        if validate_username is False:
-            new_password = user_data["password"]
-            self.sqlite_utils.update_user_data(new_username, new_password, self.username)
-            self.username, self.__password = new_username, new_password
-            return True
-        return False
+        try:
+            validate_username = self.sqlite_utils.validate_username(new_username)
+            if validate_username is False:
+                new_password = user_data["password"]
+                self.sqlite_utils.update_user_data(new_username, new_password, self.username)
+                self.username, self.__password = new_username, new_password
+                return True
+            return False
+        except Exception as e:
+            self.logger.debug(f"Error: {e}")
+            return False
 
     def delete_user(self, user_confirmation):
-        if user_confirmation is not "y":
+        if user_confirmation != "y":
             return False
-        self.sqlite_utils.delete_user_from_db(self.username)
-        return True
+        try:
+            self.sqlite_utils.delete_user_from_db(self.username)
+            return True
+        except Exception as e:
+            self.logger.debug(f"Error: {e}")
+            return False
 
     def check_message_length(self, message):
         if len(message) > 255:
@@ -54,33 +72,49 @@ class User:
         return True
 
     def check_recipient_mailbox(self, recipient, unread_messages_bool):
-        verify_recipient_mailbox = self.sqlite_utils.user_messages_list(recipient, unread_messages_bool)
-        if verify_recipient_mailbox < 5:
-            return True
-        return False
+        try:
+            verify_recipient_mailbox = self.sqlite_utils.user_messages_list(recipient, unread_messages_bool)
+            if verify_recipient_mailbox < 5:
+                return True
+            return False
+        except Exception as e:
+            self.logger.debug(f"Error: {e}")
+            return False
 
     def send_message(self, data):
         recipient = data["recipient"]
-        validate_recipient = self.sqlite_utils.validate_username(recipient)
-        if validate_recipient:
-            message = data["mail"]
-            verify_message_length = self.check_message_length(message)
-            verify_recipient_mailbox = self.check_recipient_mailbox(recipient, False)
-            if verify_message_length and verify_recipient_mailbox:
-                self.sqlite_utils.save_message_to_db(recipient, self.username, message)
-                return True
-            else:
-                return False
-        return None
+        try:
+            validate_recipient = self.sqlite_utils.validate_username(recipient)
+            if validate_recipient:
+                message = data["mail"]
+                verify_message_length = self.check_message_length(message)
+                verify_recipient_mailbox = self.check_recipient_mailbox(recipient, False)
+                if verify_message_length and verify_recipient_mailbox:
+                    self.sqlite_utils.save_message_to_db(recipient, self.username, message)
+                    return True
+                else:
+                    return False
+            return None
+        except Exception as e:
+            self.logger.debug(f"Error: {e}")
+            return False
 
     def show_unread_messages(self):
-        user_messages_list = self.sqlite_utils.user_messages_list(self.username, False)
-        self.sqlite_utils.archive_messages(self.username)
-        return user_messages_list
+        try:
+            user_messages_list = self.sqlite_utils.user_messages_list(self.username, False)
+            self.sqlite_utils.archive_messages(self.username)
+            return user_messages_list
+        except Exception as e:
+            self.logger.debug(f"Error: {e}")
+            return []
 
     def show_archived_messages(self):
-        archived_messages = self.sqlite_utils.user_messages_list(self.username, True)
-        return archived_messages
+        try:
+            archived_messages = self.sqlite_utils.user_messages_list(self.username, True)
+            return archived_messages
+        except Exception as e:
+            self.logger.debug(f"Error: {e}")
+            return []
 
     def __str__(self):
         return f"Username: {self.username}, Password: {self.__password}, Admin role: {self.admin_role}"
